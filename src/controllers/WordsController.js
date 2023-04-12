01bb53395cb7d1064987d6fa0c994c06d2e9ddbe
José Cássio <josecassio2013@gmail.com>
2023-04-12 19:10:21 -0300

    add apperror in controller

--- src/controllers/WordsController.js
+++ src/controllers/WordsController.js
@@ -2,6 +2,10 @@ class WordsController {
     create(request, response){
         const {text} = request.body;
 
+        if(!text){
+            throw new AppError("");
+        }
+
         return response.status(201).json({text});
     }
 }
